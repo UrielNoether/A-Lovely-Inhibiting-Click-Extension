09d973eecb62f72c082c3edcbcf1719110b36d8d
UrielNoether <102393947+UrielNoether@users.noreply.github.com>
2025-08-12 04:45:44 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { initStorage, modStorage, syncStorage } from "@/modules/storage";
-import { loadRemoteControl } from "@/modules/remoteControl";
+//import { loadRemoteControl } from "@/modules/remoteControl";
 import { loadSettingsMenu } from "@/modules/settingsMenu";
 import { loadCommands } from "@/modules/commands";
 import { loadDeviousPadlock } from "@/modules/deviousPadlock";
@@ -15,7 +15,8 @@ export function getModVersion(): string {
 }
 
 export function chatSendChangelog(): void {
-    const text = `<div class="dogsChangelog"><b>DOGS</b> v${getModVersion()}<br><br>Changes: <ul><li>More messages and tips.</li><li>Show member name when someone connecting remotely.</li></ul></div>`;
+    //const text = `<div class="dogsChangelog"><b>DOGS</b> v${getModVersion()}<br><br>Changes: <ul><li>More messages and tips.</li><li>Show member name when someone connecting remotely.</li></ul></div>`;
+    const text = `<div class="dogsChangelog"><b>ALICE</b> v${getModVersion()}<br><br>Changes: <ul><li>No changes, first mod version.</li><li>Hello, Alicia~</li></ul></div>`;
     messagesManager.sendLocal(text);
 }
 
@@ -30,11 +31,12 @@ style.innerHTML = css;
 document.head.append(style);
 
 registerCore({
-    name: "DOGS",
-    fullName: "Devious Obligate Great Stuff",
-    key: "DOGS",
+    name: "ALICE", //change from "DOGS"
+    fullName: "A Lovely Inhibiting Click Extension", //change from "Devious Obligate Great Stuff"
+    key: "ALICE", //change from "DOGS"
     version: getModVersion(),
-    repository: "https://github.com/FurryZoi/Devious-Obligate-Great-Stuff.git",
+    //repository: "https://github.com/FurryZoi/Devious-Obligate-Great-Stuff.git",
+    repository: "https://github.com/UrielNoether/A-Lovely-Inhibiting-Click-Extension.git",
     fontFamily: "Comfortaa"
 });
 
@@ -47,7 +49,7 @@ waitFor(() => typeof window.Player?.MemberNumber === "number").then(() => {
         loadDeviousPadlock();
         console.log(`Ready! v${getModVersion()}`);
         toastsManager.success({
-            title: `DOGS loaded`,
+            title: `ALICE loaded`, //change from 'DOGS loaded'
             message: `v${getModVersion()}`,
             duration: 4000
         });
