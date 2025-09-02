ede9c1f4511420c44fb2613ee1d49b34fb8fbde0
UrielNoether <102393947+UrielNoether@users.noreply.github.com>
2025-09-03 00:50:28 +0200

    Update deviousPadlockSubscreen.ts

--- src/subscreens/deviousPadlockSubscreen.ts
+++ src/subscreens/deviousPadlockSubscreen.ts
@@ -51,10 +51,33 @@ export class DeviousPadlockSubscreen extends BaseSubscreen {
         this.createText({
             text: "This padlock is designed such a way that there is absolutely no way to remove it, except through unlocking by either the owner of the padlock (the person that placed it) or any of the players listed as also having the keys.",
             x: 900,
-            y: 600,
+            y: 450,
             width: 800,
             withBackground: true,
             padding: 2
         });
+
+        this.createButton({
+            text: "Emergency Reset",
+            anchor: "bottom-right",
+            x: 80,
+            y: 70,
+            padding: 3,
+            width: 600,
+            icon: "Icons/ServiceBell.png",
+            isDisabled: () => True,
+            onClick: () => {
+                Object.keys(modStorage.deviousPadlock.itemGroups).forEach((k: AssetGroupItemName) => {
+                    modStorage.deviousPadlock.itemGroups[k] = Object.fromEntries(
+                        ["owner", "item"].map((n) => [n, modStorage.deviousPadlock.itemGroups[k][n]])
+                    ) as {
+                        item: SavedItem
+                        owner: number
+                    };
+                });
+                toastsManager.success({
+                    message: "Devious padlocks configurations have been successfully reset",
+                    duration: 4500
+                });
     }
 }
